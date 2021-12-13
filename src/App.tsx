1ec6dbfcbe17a55c3b68687b10a77b52c6a10114
LeonShus <leonshustov1995@gmail.com>
2021-12-13 21:15:08 +0300

    Styled Skills component

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,14 @@
 import React from "react";
-// import classes from "./App.module.css"
+import styles from "./App.module.css"
 import {Header} from "./Components/Header/Header";
 import {Skills} from "./Components/Skills/Skills";
 
 export const App = () => {
     return (
-        <div>
+        <div className={styles.container}>
             <Header/>
 
             <Skills/>
-
-
         </div>
     );
 }
---
+++ src/Components/Skills/Skill/Skill.tsx
@@ -0,0 +1,15 @@
+import React from "react"
+import defImgForSkill from "../../../default/img/React_IMG.jpeg"
+import styles from './Skill.module.css'
+
+export const Skill = () => {
+    return (
+        <div className={styles.skillContainer}>
+            <img src={defImgForSkill} alt="skill"/>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing
+                elit. Commodi corporis ipsam quia quisquam quo recusandae!
+            </p>
+        </div>
+    )
+}
