8250f16e3d6aaaac61341fc9ca5a9d044555a1d4
LeonShus <leonshustov1995@gmail.com>
2021-12-13 17:23:34 +0300

    Create Header, Nav, Main

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import classes from "./App.module.css"
+import {Header} from "./Components/Header/Header";
+import {Main} from "./Components/Main/Main";
 
 export const App = () => {
-  return (
-    <div >
-      Resume
-    </div>
-  );
+    return (
+        <div>
+            <Header/>
+            <Main/>
+        </div>
+    );
 }
