5bce4ab0ba59f4863fba1b56253c28e3b2773b9c
LeonShus <leonshustov1995@gmail.com>
2022-01-24 14:05:56 +0300

    Add redux libraries + svg icons

--- src/Components/Skills/Skill/Skill.tsx
+++ src/Components/Skills/Skill/Skill.tsx
@@ -1,11 +1,13 @@
 import React from "react"
 import defImgForSkill from "../../../common/c3-img/React_IMG.jpeg"
 import styles from "./Skill.module.scss"
+import  { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faReact } from "@fortawesome/free-brands-svg-icons"
 
 export const Skill = () => {
     return (
         <div className={styles.skillContainer}>
-            <img src={defImgForSkill} alt="skill"/>
+            <FontAwesomeIcon icon={faReact} size={"6x"} color={'blue'}/>
             <p>
                 Lorem ipsum dolor sit amet, consectetur adipisicing
                 elit. Commodi corporis ipsam quia quisquam quo recusandae!
